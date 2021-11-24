582a63cc1e4be91aff5a5f66e7cc68c582318a97
MThos <MThos@users.noreply.github.com>
2021-11-24 05:49:33 -0500

    skills paragraph added. league census project added.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,8 +4,35 @@ class Projects extends Component {
     render() {
         return (
             <div id="projects-section">
-                <h3 className="section-header">Projects</h3>
-                <p>Projects</p>
+                <div className="project-example">
+                    <div className="project-title">
+                        League Census
+                    </div>
+                    <div className="project-link">
+                        <a href="https://leaguecensus.com" target="_blank" 
+                rel="noreferrer">leaguecensus.com</a>
+                    </div>
+                    <div className="project-desc">
+                    League Census is a Django web application for retrieval of summoner, champion, item and server status information by pulling data from Riot Games API. The application is a work in progress and is built from code I had originally written in PHP and JavaScript many years ago and I am now in the process of converting it to Django. It is constantly being improved and updated as I find the time between other projects and work. Any data is stored in MongoDB and comes directly from the File System flat json files provided by Riot Games Data Dragon website or by calls directly to Riot Games API &#8212; or Redis cache. Caching of data is handled by both Redis and Cloudflare.<br/><br/>
+                    <div className="project-git">
+                        <img className="project-git-icon" src="/images/github.png" alt="Github" />
+                        <a href="https://github.com/MThos/lolcensus" target="_blank" rel="noreferrer">Github Repository</a>
+                    </div>
+                    <br/>
+                    <div className="bold ul-header">Technical Specifications:</div>
+                    <ul>
+                        <li>Django</li>
+                        <li>Python</li>
+                        <li>MongoDB</li>
+                        <li>Redis</li>
+                        <li>Nginx</li>
+                        <li>Gunicorn</li>
+                        <li>Digital Ocean &#8212; 1x vCPU / 2GB RAM</li>
+                        <li>Cloudflare &#8212; DNS / Firewall/ Caching / SSL</li>
+                    </ul>
+                    </div>
+                    <img className="project-image" src="/images/leaguecensus.png" alt="League Census" />                    
+                </div>
             </div>
         );
     }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,16 +4,22 @@ class Skills extends Component {
     render() {
         return (
             <div id="skills-section">
+                <div>
+                    <div className="setup-title">WOW! Look at all those skills!</div>
+                    <p>I have used and am very familiar with many area's of the infotech sector, not only strictly development. I have acquired some of these skills from before I was high school, some in college, some on the job and some in the last month. I'm always learning, as the field is ever changing and if you lag behind, you will quickly find yourself left behind or playing catch up.</p>
+                    <p>Skills marked with a red asterick are ones I am currently in the process of trying to use daily and master &#8212; not just simply understand how to use it, but the fundamentals behind it as well.</p>
+                </div>
+                <br/>
                 <div className="flex">
                     <div className="column-one">
                         <div className="bold">Languages</div>
                         <ul>
-                            <li>JavaScript</li>
+                            <li>JavaScript<span class="asterick bold">*</span></li>
                             <li>PHP</li>
                             <li>Python</li>
                             <li>HTML5</li>
                             <li>CSS3</li>
-                            <li>SCSS</li>
+                            <li>SCSS / SASS</li>
                             <li>Visual Basic</li>
                             <li>C#</li>
                         </ul>
@@ -21,9 +27,10 @@ class Skills extends Component {
                     <div className="column-two">
                         <div className="bold">Frameworks</div>
                         <ul>
-                            <li>ReactJS</li>
+                            <li>ReactJS<span class="asterick bold">*</span></li>
                             <li>Laravel</li>
                             <li>jQuery</li>
+                            <li>jQueryUI</li>
                             <li>Bootstrap</li>
                             <li>Flask</li>
                             <li>Django</li>
@@ -36,8 +43,8 @@ class Skills extends Component {
                             <li>SQL Server</li>
                             <li>MySQL</li>
                             <li>MariaDB</li>
-                            <li>MongoDB</li>
-                            <li>Firebase</li>
+                            <li>MongoDB<span class="asterick bold">*</span></li>
+                            <li>PostgreSQL</li>
                         </ul>
                     </div>        
                     <div>
@@ -45,22 +52,25 @@ class Skills extends Component {
                         <ul>
                             <li>Digital Ocean</li>
                             <li>Amazon AWS</li>
-                            <li>Microsoft Azure</li>
-                            <li>Heroku</li>
+                            <li>Microsoft Azure<span class="asterick bold">*</span></li>
+                            <li>Heroku<span class="asterick bold">*</span></li>
+                            <li>Docker</li>
+                            <li>Apache / Nginx</li>
                             <li>LAMP</li>
                             <li>WAMP</li>
                             <li>Cloudflare</li>
-                            <li>SEO</li>
                         </ul>
                     </div>
                     <div>
                         <div className="bold">Other</div>
                         <ul>
-                            <li>Git</li>
-                            <li>TFS</li>
+                            <li>Git / Github</li>
+                            <li>Microsoft TFS</li>
+                            <li>SEO</li>
                             <li>Visual Studio</li>
                             <li>Unit Testing</li>
                             <li>Cybersecurity</li>
+                            <li>CCNA Certificate</li>
                         </ul>
                     </div>       
                 </div>
